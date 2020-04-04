ecdc1c0e6cfd853d0fc555f9054e39a91c93c2ad
mao <maoyuan@gmail.com>
2020-04-03 22:01:18 -0400

    init

---
+++ src/test/java/com/maotion/microconfig/MicroconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maotion.microconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroconfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
